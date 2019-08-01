f1ced1a665e7c6aed25ed5e1b889e07111b65b1d
zhaojt17 <zhaojt17@mails.tsinghua.edu.cn>
2019-08-01 16:46:11 +0800

    Merge branch 'nightly' of github.com:persistever/RLLaneChange into nightly
    # Conflicts:
    #	.gitignore

--- lane_change_env.py
+++ lane_change_env.py
@@ -141,7 +141,7 @@ class Env:
                     self.sumo_step += 1
                     current_step += 1
                 if self.ego_vehicle.check_change_lane_successful():
-                    reward += 20
+                    reward += 10
                     info['endState'] = 'Change to the target gap successful'
                 else:
                     if current_step >= TIME_OUT:
@@ -197,9 +197,9 @@ class Env:
 
         speed_after = self.ego_vehicle.get_speed()
         if speed_after > speed_before:
-            reward += 5
-        elif speed_after < speed_before - 5:
-            reward -= 5
+            reward += (speed_after - speed_before) * 3
+        elif speed_after < speed_before:
+            reward += (speed_after - speed_before) * 1
 
         if self.sumo_step > 1e5 or traci.simulation.getMinExpectedNumber() <= 0 \
                 or self.ego_vehicle.is_outof_map() or self.ego_vehicle.check_outof_road():
